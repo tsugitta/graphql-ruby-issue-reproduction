efca09fedfe0cb483559c24dc31f2b7f4f0dde2d
Toshinori Tsugita <tsugita.dev@gmail.com>
2025-01-02 18:18:57 +0900

    Configure graphql-ruby-fragment_cache

--- app/graphql/graphql_ruby_issue_reproduction_schema.rb
+++ app/graphql/graphql_ruby_issue_reproduction_schema.rb
@@ -7,6 +7,8 @@ class GraphqlRubyIssueReproductionSchema < GraphQL::Schema
   # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
   use GraphQL::Dataloader
 
+  use GraphQL::FragmentCache
+
   # GraphQL-Ruby calls this when something goes wrong while running a query:
   def self.type_error(err, context)
     # if err.is_a?(GraphQL::InvalidNullError)
