7d65f9c57248dbc24de0c8e9f4215aec05a03d06
Toshinori Tsugita <tsugita.dev@gmail.com>
2025-01-08 03:55:13 +0900

    Add test to check if exist? is called N times

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -7,10 +7,11 @@ module Types
 
     field :posts, [ Types::PostType ], null: false do
       argument :use_cache, Boolean, required: true
+      argument :use_dataloader, Boolean, required: true
     end
-    def posts(use_cache:)
+    def posts(use_cache:, use_dataloader:)
       if use_cache
-        cache_fragment(expires_in: 1.minute, dataloader: true) do
+        cache_fragment(expires_in: 1.minute, dataloader: use_dataloader) do
           dataloader.with(Sources::PostsByUser).load(object.id)
         end
       else
--- spec/requests/graphql_spec.rb
+++ spec/requests/graphql_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'FragmentCache + Dataloader N+1 Investigation', type: :request do
+  let(:users) { 5.times.map { |i| User.create!(name: "User #{i}") } }
+
   before do
-    user1 = User.create!(name: "Alice")
-    user2 = User.create!(name: "Bob")
-    5.times do |i|
-      user1.posts.create!(title: "Alice Post #{i}")
-      user2.posts.create!(title: "Bob Post #{i}")
+    users.each do |user|
+      2.times do |i|
+        user.posts.create!(title: "Post #{i}")
+      end
     end
   end
 
   let(:query) do
     <<~GRAPHQL
-      query($useCache: Boolean!) {
+      query($useCache: Boolean!, $useDataloader: Boolean!) {
         users {
           id
           name
-          posts(useCache: $useCache) {
+          posts(useCache: $useCache, useDataloader: $useDataloader) {
             id
             title
           }
@@ -25,7 +26,7 @@ RSpec.describe 'FragmentCache + Dataloader N+1 Investigation', type: :request do
     GRAPHQL
   end
 
-  describe 'N+1 for database' do
+  describe.skip 'N+1 for database' do
     context 'when cache_fragment is disabled' do
       it 'should have minimal query count with effective batching' do
         queries = []
@@ -40,7 +41,7 @@ RSpec.describe 'FragmentCache + Dataloader N+1 Investigation', type: :request do
         ActiveSupport::Notifications.subscribed(counter, "sql.active_record") do
           post '/graphql', params: {
             query: query,
-            variables: { useCache: false }.to_json
+            variables: { useCache: false, useDataloader: false }.to_json
           }
         end
 
@@ -74,7 +75,7 @@ RSpec.describe 'FragmentCache + Dataloader N+1 Investigation', type: :request do
         ActiveSupport::Notifications.subscribed(counter, "sql.active_record") do
           post '/graphql', params: {
             query: query,
-            variables: { useCache: true }.to_json
+            variables: { useCache: true, useDataloader: true }.to_json
           }
         end
 
@@ -89,6 +90,88 @@ RSpec.describe 'FragmentCache + Dataloader N+1 Investigation', type: :request do
         expect(json_response["errors"]).to be_nil
         expect(json_response["data"]["users"]).to be_present
         expect(queries.size).to be 2
+      end
+    end
+  end
+
+  describe 'N+1 for cache' do
+    class LoggingMemoryStore < ActiveSupport::Cache::MemoryStore
+      attr_reader :read_multi_calls, :exist_calls
+
+      def initialize
+        super
+        @read_multi_calls = []
+        @exist_calls = []
+      end
+
+      def read_multi(*names)
+        @read_multi_calls << names
+        super
+      end
+
+      def exist?(name, options = nil)
+        @exist_calls << name
+        super
+      end
+    end
+
+    let(:cache_store) { LoggingMemoryStore.new }
+
+    before do
+      GraphQL::FragmentCache.cache_store = cache_store
+    end
+
+    context 'without dataloader option' do
+      it 'calls 1 read_multi' do
+        post '/graphql', params: {
+          query: query,
+          variables: { useCache: true, useDataloader: false }.to_json
+        }
+
+        expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        expect(json_response["errors"]).to be_nil
+        expect(json_response["data"]["users"]).to be_present
+
+        expect(cache_store.read_multi_calls.size).to be 1
+        expect(cache_store.exist_calls.size).to be 0
+
+        puts "\n=== Cache operations without dataloader ==="
+        puts "read_multi calls:"
+        cache_store.read_multi_calls.each.with_index(1) do |names, i|
+          puts "  #{i}. #{names.join(', ')}"
+        end
+        puts "exist? calls:"
+        cache_store.exist_calls.each.with_index(1) do |name, i|
+          puts "  #{i}. #{name}"
+        end
+      end
+    end
+
+    context 'with dataloader option' do
+      it 'calls 1 read_multi and N exist?' do
+        post '/graphql', params: {
+          query: query,
+          variables: { useCache: true, useDataloader: true }.to_json
+        }
+
+        expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        expect(json_response["errors"]).to be_nil
+        expect(json_response["data"]["users"]).to be_present
+
+        expect(cache_store.read_multi_calls.size).to be 1
+        expect(cache_store.exist_calls.size).to be users.size
+
+        puts "\n=== Cache operations with dataloader ==="
+        puts "read_multi calls:"
+        cache_store.read_multi_calls.each.with_index(1) do |names, i|
+          puts "  #{i}. #{names.join(', ')}"
+        end
+        puts "exist? calls:"
+        cache_store.exist_calls.each.with_index(1) do |name, i|
+          puts "  #{i}. #{name}"
+        end
       end
     end
   end
