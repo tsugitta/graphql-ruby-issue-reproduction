8b17847ca5776bbf7e902297b4e6344799a98f01
Toshinori Tsugita <tsugita.dev@gmail.com>
2025-01-08 03:45:47 +0900

    Refactor GraphQL N+1 tests for clarity and consistency

--- spec/requests/graphql_spec.rb
+++ spec/requests/graphql_spec.rb
@@ -25,69 +25,71 @@ RSpec.describe 'FragmentCache + Dataloader N+1 Investigation', type: :request do
     GRAPHQL
   end
 
-  context 'when cache_fragment is disabled' do
-    it 'should have minimal query count with effective batching' do
-      queries = []
+  describe 'N+1 for database' do
+    context 'when cache_fragment is disabled' do
+      it 'should have minimal query count with effective batching' do
+        queries = []
 
-      counter = ->(_name, _started, _finished, _unique_id, payload) do
-        sql = payload[:sql].to_s
-        if sql.start_with?("SELECT")
-          queries << sql
+        counter = ->(_name, _started, _finished, _unique_id, payload) do
+          sql = payload[:sql].to_s
+          if sql.start_with?("SELECT")
+            queries << sql
+          end
         end
-      end
 
-      ActiveSupport::Notifications.subscribed(counter, "sql.active_record") do
-        post '/graphql', params: {
-          query: query,
-          variables: { useCache: false }.to_json
-        }
-      end
+        ActiveSupport::Notifications.subscribed(counter, "sql.active_record") do
+          post '/graphql', params: {
+            query: query,
+            variables: { useCache: false }.to_json
+          }
+        end
 
-      puts "\n=== Queries without cache ==="
-      queries.each.with_index(1) do |sql, i|
-        puts "\n#{i}. #{sql}"
-      end
+        puts "\n=== Queries without cache ==="
+        queries.each.with_index(1) do |sql, i|
+          puts "\n#{i}. #{sql}"
+        end
 
-      expect(response).to have_http_status(:success)
-      json_response = JSON.parse(response.body)
-      expect(json_response["errors"]).to be_nil
-      expect(json_response["data"]["users"]).to be_present
-      expect(queries.size).to be 2
+        expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        expect(json_response["errors"]).to be_nil
+        expect(json_response["data"]["users"]).to be_present
+        expect(queries.size).to be 2
+      end
     end
-  end
 
-  context 'when cache_fragment is enabled' do
-    it 'should have minimal query count with effective batching' do
-      GraphQL::FragmentCache.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+    context 'when cache_fragment is enabled' do
+      it 'should have minimal query count with effective batching' do
+        GraphQL::FragmentCache.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 
-      queries = []
+        queries = []
 
-      counter = ->(_name, _started, _finished, _unique_id, payload) do
-        sql = payload[:sql].to_s
-        if sql.start_with?("SELECT")
-          queries << sql
+        counter = ->(_name, _started, _finished, _unique_id, payload) do
+          sql = payload[:sql].to_s
+          if sql.start_with?("SELECT")
+            queries << sql
+          end
         end
+
+
+        ActiveSupport::Notifications.subscribed(counter, "sql.active_record") do
+          post '/graphql', params: {
+            query: query,
+            variables: { useCache: true }.to_json
+          }
+        end
+
+        puts "\n=== Queries with cache ==="
+        queries.each.with_index(1) do |sql, i|
+          puts "\n#{i}. #{sql}"
+        end
+
+
+        expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        expect(json_response["errors"]).to be_nil
+        expect(json_response["data"]["users"]).to be_present
+        expect(queries.size).to be 2
       end
-
-
-      ActiveSupport::Notifications.subscribed(counter, "sql.active_record") do
-        post '/graphql', params: {
-          query: query,
-          variables: { useCache: true }.to_json
-        }
-      end
-
-      puts "\n=== Queries with cache ==="
-      queries.each.with_index(1) do |sql, i|
-        puts "\n#{i}. #{sql}"
-      end
-
-
-      expect(response).to have_http_status(:success)
-      json_response = JSON.parse(response.body)
-      expect(json_response["errors"]).to be_nil
-      expect(json_response["data"]["users"]).to be_present
-      expect(queries.size).to be 2
     end
   end
 end
